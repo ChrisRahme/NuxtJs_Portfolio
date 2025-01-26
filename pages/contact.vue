496d9c7a3259cb23d73cdf47bd4139eb465bf04b
Chris Rahmé <chris.rahme@hotmail.com>
2025-01-26 22:06:22 +0200

    Commit

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,26 +1,10 @@
 <template>
     <div>
         <h1>Contact</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rem repudiandae, eveniet amet impedit inventore laudantium dolore repellat laborum
-            sunt!
-        </p>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat rem repudiandae, eveniet amet impedit inventore laudantium dolore repellat laborum
-            sunt!
-        </p>
+        <p>Coming soon!</p>
     </div>
 </template>
 
 <script setup></script>
 
-<style scoped>
-    h1 {
-        margin-bottom: 20px;
-        font-size: 36px;
-    }
-
-    p {
-        margin: 20px 0;
-    }
-</style>
+<style scoped></style>
